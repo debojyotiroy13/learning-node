d09bc33888a4403328622e798cdc2633f4465577
debojyotiroy13 <debojyotiroy13@gmail.com>
2018-06-30 22:12:49 +0530

    Added error handling to for file extention , request method not supported and file not found

--- my-app/index.js
+++ my-app/index.js
@@ -1,12 +1,41 @@
 // Importing requirements
 var server_conf = require('./config');
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 
 var server = http.Server((request,response)=>{ 
-    //response.writeHead(200 , {content-type: 'text/html'})
     
-    response.end('<H1>Index Page!</H1>');
+    console.log(request.headers);
+    if(request.method == 'GET'){
+        var fileUrl;
+        if(request.url == '/') fileUrl = 'index.html';
+        else fileUrl = request.url;
+        var filePath = path.resolve('./views' + fileUrl);
+        var fileExt = path.extname(filePath);
+        if(fileExt == '.html'){
+            //if(filePath.exists)
+            fs.exists(filePath , (exists) => {
+                if(!exists){
+                    response.writeHead(404,{'Content-Type' : 'text/html'});
+                    response.end('<html> <body> <h1>Error 404 : File is not found!</h1> </body></html>')           
+                    return;
+                }
+                fs.createReadStream(filePath).pipe(response);
+            })
+        }
+        else{
+            response.writeHead(404,{'Content-Type' : 'text/html'});
+            response.end('<html> <body> <h1>Error 404 : File extention ' + fileExt + ' is not supported</h1> </body></html>')   
+        }
+        
+    }
+    else{
+        response.writeHead(404,{'Content-Type' : 'text/html'});
+        response.end('<html> <body> <h1>Error 404 : Request method ' + request.method + ' is not supported</h1> </body></html>')
+    }
+    
 
 })
 
