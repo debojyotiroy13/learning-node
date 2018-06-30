fda149ccda85a3f7e1479f712b9274855905a218
debojyotiroy13 <debojyotiroy13@gmail.com>
2018-06-30 17:37:54 +0530

    added http

--- my-app/index.js
+++ my-app/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+// Importing requirements
+var server_conf = require('./config');
+var http = require('http');
+
+
+var server = http.Server((request,response)=>{ 
+    //response.writeHead(200 , {content-type: 'text/html'})
+    
+    response.end('<H1>Index Page!</H1>');
+
+})
+
+server.listen(server_conf.port,server_conf.host);
+console.log('Server started on ' + server_conf.host + ' : ' + server_conf.port);
